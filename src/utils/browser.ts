e1d514a9ea5420f43e62d95edeab72d59f8388ad
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2025-03-27 11:08:08 -0700

    fix: build

--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -1,26 +1,26 @@
 import platform from "platform";
 
 export const BROWSER_NAMES = {
-  chrome: 'Chrome',
-  firefox: 'Firefox',
-  brave: 'Brave',
-  edge: 'Edge',
-  opera: 'Opera',
-  safari: 'Safari',
+  chrome: "Chrome",
+  firefox: "Firefox",
+  brave: "Brave",
+  edge: "Edge",
+  opera: "Opera",
+  safari: "Safari",
 };
 
 export const detectOS = (): string => {
   const { userAgent } = navigator;
   const info = platform.parse(userAgent);
 
-  return info.os;
+  return info.os?.toString() || "Unknown OS";
 };
 
 export const detectBrowser = (): string => {
   const { userAgent } = navigator;
   const info = platform.parse(userAgent);
 
-  return `${info.name} ${info.version}` ;
+  return `${info.name} ${info.version}`;
 };
 
 export const copyToClipboard = async (textToCopy: string) => {
@@ -28,18 +28,27 @@ export const copyToClipboard = async (textToCopy: string) => {
     await navigator.clipboard.writeText(textToCopy);
     return true;
   } catch (err) {
-    console.error('Failed to copy: ', err);
+    console.error("Failed to copy: ", err);
   }
-}
+};
 
-export const openSolscanExplorerAddress = (address: string, cluster: string) => {
-  window.open(`https://solscan.io/account/${address}?cluster=${cluster}`, '_blank');
-}
+export const openSolscanExplorerAddress = (
+  address: string,
+  cluster: string
+) => {
+  window.open(
+    `https://solscan.io/account/${address}?cluster=${cluster}`,
+    "_blank"
+  );
+};
 
 export const openSolscanExplorerTransaction = (id: string, cluster: string) => {
-  window.open(`https://solscan.io/tx/${id}?cluster=${cluster}`, '_blank');
-}
+  window.open(`https://solscan.io/tx/${id}?cluster=${cluster}`, "_blank");
+};
 
 export const openContactSupport = () => {
-  window.open(`mailto:support@enkrypt.com?subject=Enkrypt Staking Dapp Enquiry - ${__PACKAGE_VERSION__} - ${detectBrowser()} - ${detectOS()}`, '_blank');
-}
+  window.open(
+    `mailto:support@enkrypt.com?subject=Enkrypt Staking Dapp Enquiry - ${__PACKAGE_VERSION__} - ${detectBrowser()} - ${detectOS()}`,
+    "_blank"
+  );
+};
